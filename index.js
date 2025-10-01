e8c6abdb56c4d0403ee001935e3ced841160a9fb
FrancoZeg <101823582+FrancoZeg@users.noreply.github.com>
2025-10-01 00:25:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ import path from 'path';
 import userRouter from './routes/user.route.js';
 
 import session from 'express-session';
-
 import passport from './middlewares/passport.js';
-
 import { swaggerUiMiddleware, swaggerUiSetup } from './swagger.js';
-
 import adminRouter from './routes/admin.route.js';
 import estudianteRouter from './routes/estudainte.toute.js';
+
 const app = express();
 
 app.use(
@@ -30,21 +28,27 @@ app.use(
     credentials: true,
   })
 );
-//console.log = function () {};
+
 app.use(cookieParser(process.env.FIRMA_cokie));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-//app.use(express.urlencoded({ extended: true }))
-//app.use(express.static('public'))
 
-//app.use('/', publicRouter)
+// ====== 🔀 FIX para /undefined/... ======
+app.get('/undefined/api/auth/google', (req, res) => {
+  return res.redirect('/api/auth/google');
+});
+
+app.get('/undefined/api/auth/google/callback', (req, res) => {
+  return res.redirect('/api/auth/google/callback');
+});
+// =======================================
+
+// Rutas de tu app
 app.use('/api/estudiante', estudianteRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api', userRouter);
-//app.use('/cedhi/tutor', petRouter)
 
 app.use('/docs', swaggerUiMiddleware, swaggerUiSetup);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log('Servidor andando en ' + PORT));
